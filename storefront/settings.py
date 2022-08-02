f7e6b3c3938602384b6cfda308f62056694dc546
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-08-02 09:08:22 +0430

    changed db from sql3 to mysql

--- storefront/settings.py
+++ storefront/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.mysql',
+        'NAME'    : 'storefront',
+        'USER'    : 'root',
+        'PASSWORD': '0011605375nA',
+        'HOST'    : 'localhost',
+        'PORT'    : '3306',
     }
 }
 
