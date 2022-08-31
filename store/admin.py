3b7b53050656258a5fa67d7ec1f22b8145630971
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-08-31 08:17:33 +0430

    registered Collection in admin module

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import *
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     pass
+
+
