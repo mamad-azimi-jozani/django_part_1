7d55029e9f634e62b5c18359d9a68a6da66218be
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-08-31 08:31:53 +0430

    added some customizing option to Customer

--- store/admin.py
+++ store/admin.py
@@ -13,4 +13,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['unit_price']
     list_per_page = 20
 
-
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 20
+    ordering = ['first_name', 'last_name']
