f4ae726c052bf817485cf7f4b95786d199a485d4
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-08-31 08:47:46 +0430

    created func for computed column to Product which alert message if their inventory is lass than 10 or not

--- store/admin.py
+++ store/admin.py
@@ -9,9 +9,14 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 20
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return "Ok"
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
