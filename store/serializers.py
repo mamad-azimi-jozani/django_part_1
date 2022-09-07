9f8a0d4b189614e7ef12af908aedf79e6b2eb1cb
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-07 15:33:41 +0430

    created CollectionSerializer for serializing Collection model

--- store/serializers.py
+++ store/serializers.py
@@ -9,3 +9,8 @@ class ProductSerializer(serializers.ModelSerializer):
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
