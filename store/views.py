03012c701792b4103cb563d225cbdbde685aa7df
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-07 07:55:03 +0430

    wrote correct api for product-list and product_detail

--- store/views.py
+++ store/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import ProductSerializer
+from .models import *
 @api_view()
 def product_list(request):
-    return Response('Ok')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    return Response(f'Ok {id}')
+    product = Product.objects.get(id=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
