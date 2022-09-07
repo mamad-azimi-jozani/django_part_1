8ed3d0255c381542316a37ed55afd4f835f9f779
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-07 09:21:53 +0430

    added put request to product_detail

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import *
+from rest_framework import status
 @api_view(['GET', 'POST'])
 def product_list(request):
     if request.method == 'GET':
@@ -13,10 +14,16 @@ def product_list(request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response('Ok')
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view()
+@api_view(['GET', 'PUT'])
 def product_detail(request, id):
     product = Product.objects.get(id=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
