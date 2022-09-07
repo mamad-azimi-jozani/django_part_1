e2b30f4e9d232394f50130bffb4130c562b84110
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-07 08:23:51 +0430

    created post request for product_list

--- store/views.py
+++ store/views.py
@@ -3,11 +3,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import *
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            print(serializer.validated_data)
+            return Response('Ok')
 
 @api_view()
 def product_detail(request, id):
