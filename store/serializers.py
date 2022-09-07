cfd468711ce32f2f1756fdc3d49b3964ba4a9156
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-07 08:51:26 +0430

    added certain field to ProductSerializer because they can not be null

--- store/serializers.py
+++ store/serializers.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
