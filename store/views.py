0a212dc344370a08f051512a9c876b88c3f78d67
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-08 09:40:09 +0430

    created ProductList class for product_list function and ProductDetail class for product_detail

--- store/views.py
+++ store/views.py
@@ -1,35 +1,41 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ProductSerializer, CollectionSerializer
 from .models import *
 from rest_framework import status
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+
+
+class ProductList(APIView):
+    def get(self, request):
         queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, id):
-    product = Product.objects.get(id=id)
-    if request.method == 'GET':
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
+class ProductDetail(APIView):
+    def get(self, request, id):
+        product = Product.objects.get(id=id)
+        if request.method == 'GET':
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
+    def put(self, request, id):
+        product = Product.objects.get(id=id)
         serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, id):
+        product = Product.objects.get(id=id)
         if product.orderitem_set.count() > 0:
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
