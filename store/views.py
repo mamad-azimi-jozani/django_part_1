b39e8c7aae963b7064950d4212599f39cb43205b
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-09 17:57:27 +0430

    created ModelViewSet for both collection endpoint and product endpoint

--- store/views.py
+++ store/views.py
@@ -8,13 +8,13 @@ from .models import *
 from rest_framework import status
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
-class ProductList(ListCreateAPIView):
+from rest_framework.viewsets import ModelViewSet
+
+
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
     def delete(self, request, pk):
         product = Product.objects.get(pk=pk)
         if product.orderitem_set.count() > 0:
@@ -23,19 +23,12 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(
             count_product=Count('product')
         )
     serializer_class = CollectionSerializer
 
-
-# class CollectionDetail(RetrieveUpdateDestroyAPIView):
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(
-        count_product=Count('product'))
-    serializer_class = CollectionSerializer
     def delete(self, request, pk):
         collection = get_object_or_404(Collection, pk=pk)
         if collection.product_set.count() > 0:
@@ -45,19 +38,8 @@ class CollectionDetail(RetrieveUpdateDestroyAPIView):
 
 
 
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def collection_detail(request, id):
-#     collection = get_object_or_404(Collection.objects.annotate(
-#         count_product=Count('product')), id=id)
-#
-#
-#     if request.method == 'GET':
-#         serializer = CollectionSerializer(collection)
-#         return Response(serializer.data)
-#     elif request.method == 'PUT':
-#         serializer = CollectionSerializer(collection, data=request.data)
-#         if serializer.is_valid(raise_exception=True):
-#             serializer.save()
-#             return Response(serializer.data)
-#     elif request.method == 'DELETE':
-#         serializer = CollectionSerializer(collection)
+
+
+
+
+
