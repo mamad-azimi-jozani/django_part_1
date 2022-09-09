ee03eefb878ba6aa4c700277c828556967e12c8f
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-09 11:27:23 +0430

    change url param to pk in product_detail

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('products/', ProductList.as_view()),
-    path('products/<int:id>', ProductDetail.as_view()),
+    path('products/<int:pk>', ProductDetail.as_view()),
     path('collection/', CollectionList.as_view()),
     path('collection/<int:id>', collection_detail),
 ]
