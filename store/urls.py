96e597c0e27aa6603c962decee3c3ab8bc96a2e2
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-09 10:54:47 +0430

    updated endpoint of collection with class view

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ from .views import *
 urlpatterns = [
     path('products/', ProductList.as_view()),
     path('products/<int:id>', ProductDetail.as_view()),
-    path('collection/', collection_list),
+    path('collection/', CollectionList.as_view()),
     path('collection/<int:id>', collection_detail),
 ]
