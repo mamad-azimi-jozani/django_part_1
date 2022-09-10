18a5950f87dd14521f508d6630d4eda23398724b
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-10 15:45:35 +0430

    created serializer for Review model

--- store/serializers.py
+++ store/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product, Collection
+from .models import Product, Collection, Review
 from decimal import Decimal
 from django.db.models import Count
 class CollectionSerializer(serializers.ModelSerializer):
@@ -17,4 +17,9 @@ class ProductSerializer(serializers.ModelSerializer):
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'name', 'description', 'product', 'date']
 
