37b140acd91d099cf7863d42fe60dd3d433e3dce
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-10 12:04:43 +0430

    finally override completely destroy method in CollectionViewSet

--- store/views.py
+++ store/views.py
@@ -30,9 +30,8 @@ class CollectionViewSet(ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         if Product.objects.filter(collection__id=instance.id).count() > 0:
-            print(Product.objects.filter(collection__id=instance.id).count())
             return Response({'error': 'product can not be deleted'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
+        instance.delete()
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
 
