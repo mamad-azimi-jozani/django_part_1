ce35732fefefe615c073c3d1a3efbad5183ccb58
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-09-11 09:39:28 +0430

    created nested url for product and it's revies

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('collection', CollectionViewSet)
 
+product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+product_router.register('reviews', ReviewViewSet, basename='product-reviews')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + product_router.urls
